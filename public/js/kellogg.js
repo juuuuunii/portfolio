205907335a41892c8523839cae2dcfc35cbcfa47
juuuuunii <01071720722s@gmail.com>
2018-12-12 17:29:48 +0900

    1212

--- public/js/kellogg.js
+++ public/js/kellogg.js
@@ -1,3 +1,4 @@
+
 var navNum = 0;
 var navNumOld = 0;
 $(".nav > li > a").mouseenter(function() {
@@ -5,11 +6,22 @@ $(".nav > li > a").mouseenter(function() {
 	navNum = $(this).index();
 	$(".nav > li > a").css({"background-color":"#fff","color":"#222"});
 	$(this).css({"background-color":"#c80136","color":"#fff"});
-	$(".nav_hover").stop().slideDown(500, function() {
-		$(".nav_hover").mouseleave(function() {
-			$(".nav_hover").stop().slideUp(500);
-			$(".nav > li > a").css({"background-color":"#fff","color":"#222"}
-			);
+	$(".nav > li").mouseenter(function() {
+		$(this).find(".nav_hover").slideDown(500, function() {
+			$(".nav_hover").mouseleave(function() {
+				$(".nav_hover").stop().slideUp(500);
+				$(".nav > li > a").css({"background-color":"#fff","color":"#222"});
+			});
 		});
 	});
 });
+/*
+$(".nav > li").mouseenter(function() {
+	$(this).find(".nav_hover").slideDown(500, function() {
+		$(".nav_hover").mouseleave(function() {
+			$(".nav_hover").stop().slideUp(500);
+			$(".nav > li > a").css({"background-color":"#fff","color":"#222"});
+		});
+	});
+});
+*/
