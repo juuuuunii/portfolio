813723ca3b7250e3096a5432bcd96228ee5e8ba2
juuuuunii <01071720722s@gmail.com>
2018-12-12 15:47:11 +0900

    181212

---
+++ public/js/kellogg.js
@@ -0,0 +1,15 @@
+var navNum = 0;
+var navNumOld = 0;
+$(".nav > li > a").mouseenter(function() {
+	navNumOld = navNum;
+	navNum = $(this).index();
+	$(".nav > li > a").css({"background-color":"#fff","color":"#222"});
+	$(this).css({"background-color":"#c80136","color":"#fff"});
+	$(".nav_hover").stop().slideDown(500, function() {
+		$(".nav_hover").mouseleave(function() {
+			$(".nav_hover").stop().slideUp(500);
+			$(".nav > li > a").css({"background-color":"#fff","color":"#222"}
+			);
+		});
+	});
+});
